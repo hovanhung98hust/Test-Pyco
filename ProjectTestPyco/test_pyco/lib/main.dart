87c1d17eea37e870f12a58d6d634194bbde293d3
hovanhung <hunghovan288@gmail.com>
2020-04-11 22:04:17 +0700

    first commit

---
+++ ProjectTestPyco/test_pyco/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
